aed4048840fb2de2fee1d18c017530d6facd9123
tarangvats <63825607+tarangvats@users.noreply.github.com>
2021-10-29 11:50:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from snake import Snake
 from turtle import Turtle,Screen
+from food import Food
+from score import Score
 import time
 
 
 
 s = Screen()
+f = Food()
+score=Score()
+
 s.setup(height=600,width=600)
 s.bgcolor("black")
 s.title("Snake Game")
@@ -16,16 +21,27 @@ s.onkey(sn.up,key="Up")
 s.onkey(sn.down,key="Down")
 s.onkey(sn.move_right,key = "Right")
 
+a = f.xcor()
+b = f.ycor()
+sr=0
 game_is_on = True
 while game_is_on:
+
     s.update()
     time.sleep(0.1)
     sn.snake_move()
     x = sn.segments[2].xcor()
     y = sn.segments[2].ycor()
-    if x >= 290 or x <= -290 or y >= 290 or y <= -290:
+    score.clear()
+    score.print()
+    if x >= 290 or x <= -290 or y >= 250 or y <= -290:
         game_is_on = False
-        print("You Lost")
+        score.game_over()
+        print(f"Your score is {sr}")
+    if sn.segments[2].distance(f) <=15:
+        sr = sr +1
+        f.refresh()
+        score.update()
 
 
 s.exitonclick()
